dc204aa493ec8b29e0cec1b585ad46667c9842d6
CamiloHenriquez <camilo.parkour@gmail.com>
2023-11-09 21:34:57 -0300

    validators

--- PruebaN2/Cine/models.py
+++ PruebaN2/Cine/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-
+from django.core import validators
 # Create your models here.
 
 class Cine(models.Model):
     nombre = models.CharField(max_length=20)
     ubicacion = models.CharField(max_length=20)
     correo = models.EmailField(max_length=30)
-    telefono = models.IntegerField()
-    cantSalas = models.IntegerField()
-    aforo = models.IntegerField()
+    telefono = models.PositiveIntegerField(validators=[
+        validators.MinValueValidator(911111111)
+    ])
+    cantSalas = models.PositiveIntegerField(validators=[
+        validators.MinValueValidator(1),
+        validators.MaxValueValidator(100000000000)
+    ])
+    aforo = models.PositiveIntegerField(validators=[
+        validators.MinValueValidator(1),
+        validators.MaxValueValidator(100000000000)
+    ])
 
 
